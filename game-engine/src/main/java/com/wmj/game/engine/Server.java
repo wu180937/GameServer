be31d959a7d6e6917a87e3b44bb6387eefd5a0f1
wumj <254180937@qq.com>
2019-03-05 00:47:02 +0800

    1.添加maven自动生成protobuf

--- game-engine/src/main/java/com/wmj/game/engine/Server.java
+++ game-engine/src/main/java/com/wmj/game/engine/Server.java
@@ -1,15 +1,35 @@
 package com.wmj.game.engine;
 
+import com.google.common.net.HostAndPort;
+import com.orbitz.consul.Consul;
 import com.wmj.game.common.service.ServiceName;
 
 public class Server {
     private ServiceName serviceName;
+    private String consulHost;
+    private int consulPort;
 
-    public Server(ServiceName serviceName) {
+    private Server(ServiceName serviceName) {
         this.serviceName = serviceName;
     }
 
     public ServiceName getServiceName() {
+
         return serviceName;
+    }
+
+    private void Register2Consul(String serviceName, int servicePort) {
+        Consul consul = Consul.builder().withPing(true).withHostAndPort(HostAndPort.fromParts(consulHost, consulPort)).build();
+    }
+
+    public static Server.Builder builder() {
+        return new Server.Builder();
+    }
+
+    public static class Builder {
+        private ServiceName serviceName;
+        private String consulHost;
+        private int port;
+
     }
 }
