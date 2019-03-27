430e649561c75d7992b35f50cb7debd4a830aa09
wumj <254180937@qq.com>
2019-03-28 00:50:04 +0800

    1.修改websocket

--- game-web/src/main/java/com/wmj/game/web/controller/AccountController.java
+++ game-web/src/main/java/com/wmj/game/web/controller/AccountController.java
@@ -1,5 +1,8 @@
 package com.wmj.game.web.controller;
 
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -8,6 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
  * @Description:
  */
 @RestController
+@RequestMapping("/ac")
 public class AccountController {
+    @RequestMapping(value = "/ulogin", method = RequestMethod.POST)
+    public JSONObject login(String userName, String pwd) {
+        JSONObject result = new JSONObject();
+        return result;
+    }
 
 }
